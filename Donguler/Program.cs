b6799d0d8d113387dfaf8ac14f2bfc64473b89ee
Rümeysa Gedik <rumeysa1783@gmail.com>
2021-01-14 00:09:27 +0300

    First Commit

---
+++ Donguler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //array-dizi
+
+            string[] kurslar = new string[] {"Yazlım geliştirici kampı","programlamaya başlangıc kampi","Java","python","C#" }; //birden fazla string tutma
+
+            //gercekhayatta : string[] kurslar=kurslarigetir();
+
+            for (int i = 0; i <kurslar.Length; i++) //i+=2 //i'yi sıfırdan baslat, kurslar dizisinin uzunlugu kadar i'yi bir arttır.
+            {               
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For Bitti");
+
+            foreach (string kurs in kurslar) //kurslar icinde dolas
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu-footer");
+        }
+    }
+}
+//Döngüleri biribirine benzeyen islemleri tekrar etmek icin kullaniriz.
+//Tek tek tanımlamak yerine arrayda tutmak daha dinamik.
+//çogunlukla programlama dilleri saymaya sfırdan baslar
+//foreach, dizi temelli yapıları tek tek dönmeye yarar.
---
+++ KampIntro/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety:tip guvenligi
+            string kategoriEtiketi = "Kategori"; //deger tutucu. Metnin takma ismi(alias)
+
+            int ogrenciSayısı = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalis Butonu");
+            }
+            else if (dolarDun<dolarBugun) 
+            {
+                Console.WriteLine("Artis Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Degismedi Butonu");
+            }
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı Ayarlari Butonu ");
+            }
+            else
+            {
+                Console.WriteLine("Giris Yap Butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
+
+//degiskenler veri tutuculardır. Veriyi degiskenler ile yönetiriz.
+//noktalivirgul, satirin bittigini belirtir.
+//do not repeat yourself:kendini tekrarlama.
+//if'de sart gecerliyse blok calissin. kisa yol if için, if yazip iki defa tab'a basiliyor.
+//
+
